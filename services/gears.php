35385ca09873ea8c17d06fe620db375a66d44dee
Enrico Anello <enrico.anello@gmail.com>
2016-11-28 12:30:01 +0100

    Update gears.php

--- services/gears.php
+++ services/gears.php
@@ -7,24 +7,17 @@ $q = htmlspecialchars($_GET["q"]);
 $conn = pg_connect("host=db-tuna.d4science.org port=5432 dbname=sardara_world user=invsardara password=fle087");
 
 if ($q != null && $q != "") {
-	$where1 = "where LOWER(gear_labels.english_name_gear) ILIKE '%".strtolower($q)."%'";
-	$where2 = "where LOWER(geargroupcode) ILIKE '%".strtolower($q)."%'";
+	$where = "where LOWER(gear_labels.english_name_gear) ILIKE '%".strtolower($q)."%'";
 } else {
-	$where1 = "";
-	$where2 = "";
+	$where = "";
 }
 
-$query = "(select distinct " .
+$query = "select distinct " .
 "gear_labels.english_name_gear AS value, " .
 "gear_labels.english_name_gear AS label " .
 "from tunaatlas.catches_ird_rf1 " .
-"JOIN gear.gear_labels ON catches_ird_rf1.id_geargroup_standard=gear_labels.id_gear ".$where1.")  " .
-"UNION " .
-"(select distinct " .
-"geargroupcode AS value, " .
-"len_geargroup AS label " .
-"from gear.geargroup_tunaaltas ".$where2.") " .
-"order by label";
+"JOIN gear.gear_labels ON  " .
+"catches_ird_rf1.id_geargroup_standard=gear_labels.id_gear  ".$where." order by label";
 
 $result = pg_query($conn, $query);
 $out = array();
@@ -35,6 +28,4 @@ while ($row = pg_fetch_row($result)) {
 	array_push($out, $o);
 }
 print(json_encode($out));
-
-
 ?>
